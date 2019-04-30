993455c53bda22bf78943e0a414b0e4e44de2abe
github1 <john.collier4@gmail.com>
2019-04-30 00:13:05 -0400

    build es5 dist

--- bin/build-lib.sh
+++ bin/build-lib.sh
@@ -12,14 +12,19 @@ rm -rf "${DIST}"
 if [[ -f "tsconfig.json" ]]; then
     TSC=$(dirname $(dirname $(node -p "require.resolve('typescript')")))/bin/tsc
     "${TSC}" --pretty --project tsconfig.json
+    "${TSC}" --pretty --project tsconfig.json --module "commonjs" --outDir "${DIST}/es5"
 else
     IGNORE="src/**/*.test.js"
     cp "${SCRIPTPATH}/../config/babelrc.json" .babelrc
     ${SCRIPTPATH}/../node_modules/.bin/babel src \
         --out-dir "${DIST}" \
         --ignore "${IGNORE}"
+    sed -i '' 's|false|"commonjs"|g' .babelrc
+    ${SCRIPTPATH}/../node_modules/.bin/babel src \
+        --out-dir "${DIST}/es5" \
+        --ignore "${IGNORE}"
     rm .babelrc
 fi
 
 MAIN=$(cat package.json | jq -r '.main' | sed 's|dist/||g')
-cat package.json | jq '.main = "'"${MAIN}"'" | .module = "'"${MAIN}"'"' > "${DIST}/package.json"
+cat package.json | jq '.main = "es5/'"${MAIN}"'" | .module = "'"${MAIN}"'"' > "${DIST}/package.json"
