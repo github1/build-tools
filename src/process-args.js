873d4b51e8474e6ef01e22697f0939cc19f9ed9b
github1 <john.collier4@gmail.com>
2019-08-02 23:45:32 -0400

    transpile react-redux-common-modules
    ref-7d64f85f56158bf2da2582bc10b6b596e8d4d4d7

--- src/process-args.js
+++ src/process-args.js
@@ -1,5 +1,24 @@
 module.exports = process => {
-    return process.argv.slice(0).filter((item, index) => {
-        return index > 1;
+    var task;
+    var prevFlag;
+    var processArgs = process.argv.slice(0);
+    processArgs.shift();
+    while(true) {
+        if (/^-/.test(processArgs[0]) || processArgs.length == 0) {
+            break;
+        } else {
+            task = processArgs.shift();
+        }
+    }
+    return processArgs.reduce((args, arg) => {
+        if (/^-/.test(arg)) {
+            prevFlag = arg.replace(/^[-]+/, '');
+            args[prevFlag] = true;
+        } else if (prevFlag) {
+            args[prevFlag] = arg;
+        }
+        return args;
+    }, {
+        task: task
     });
 };
