bab3742a9900230f8bfe825efec940fdef9f811a
github1 <john.collier4@gmail.com>
2019-08-08 23:02:12 -0400

    add type definitions for jest helpers

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+export interface DeferredPromiseConfig {
+  onResolve? : (result : any) => any;
+  onReject? : (error : Error) => any;
+}
+
+export declare class DeferredPromise extends Promise<any> {
+  constructor(executor : (resolve : (value? : any | PromiseLike<any>) => void, reject : (reason? : any) => void) => void);
+
+  public static create(config? : DeferredPromiseConfig) : DeferredPromise;
+
+  public forceResolve(value : any) : Promise<any>;
+
+  public forceReject(value : any) : Promise<any>;
+}
+
+export declare function findJson(json : any, func : (json : any) => boolean, matches? : Array<any>) : any;
+
+export declare function withAttribute(key : string, value : any) : (json : any) => boolean;
