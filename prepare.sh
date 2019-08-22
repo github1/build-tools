6a7d431fb66a9c3dec18a5f284ffed85c40f8c89
github1 <john.collier4@gmail.com>
2019-08-22 00:58:29 -0400

    add aux prepare to build-tools

--- prepare.sh
+++ prepare.sh
@@ -3,4 +3,7 @@
 DIST="./dist"
 rm -rf "${DIST}"
 mkdir -p "${DIST}"
-cp -Rv bin config src index.js index.d.ts package.json "${DIST}/"
+cp -Rv bin config src index.js index.d.ts package.json README.md "${DIST}/"
+if [[ -f "${AUX_PREPARE_SCRIPT}" ]]; then
+  "${AUX_PREPARE_SCRIPT}" "${DIST}/"
+fi
