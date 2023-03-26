cb04f0df7aebca8fbdcaeea687d8c96baf097fdc
github1 <john.collier4@gmail.com>
2023-03-26 23:39:31 +0000

    use --no-bin-links when installing deps

--- bin/package-server.sh
+++ bin/package-server.sh
@@ -3,7 +3,7 @@
 set -e
 
 function verify() {
-  if [[ "${TEST}" != "false" ]] && [[ "${skip_tests}" != "false" ]]; then
+  if [[ "${TEST}" != "false" && "${skip_tests}" != "true"  ]] && [[ "${skip_tests}" != "false" ]]; then
     echo "Verifying"
     npm run -s test
     npm run -s lint
@@ -14,25 +14,24 @@ function verify() {
 
 function clean() {
   echo "Cleaning"
-  mkdir -p target/dist/public
-  rm -rf target/dist/public/*
-  rm -rf target/dist/*.tar.gz
+  rm -rf ./target/dist
+  mkdir -p ./target/dist
 }
 
-function installAppServer() {
+function install_app_server() {
   echo "Installing appServer"
-  mkdir -p target/dist/public/app-server
-  APP_SERVER_BASE=$(node -p 'const path = require("path"); path.dirname(path.dirname(require.resolve("@github1/app-server")));')
-  if [[ ! -d "${APP_SERVER_BASE}" ]]; then
-    APP_SERVER_BASE=../../packages/app-server
+  mkdir -p ./target/dist/public/app-server
+  app_server_base_dir=$(node -p 'const path = require("path"); path.dirname(path.dirname(require.resolve("@github1/app-server")));')
+  if [[ ! -d "${app_server_base_dir}" ]]; then
+    app_server_base_dir=../../packages/app-server
   fi
-  cp -R "${APP_SERVER_BASE}/assets" target/dist/public/app-server/assets
-  cp -R "${APP_SERVER_BASE}/bin" target/dist/public/app-server/bin
-  cp -R "${APP_SERVER_BASE}/src" target/dist/public/app-server/src
-  cp -R "${APP_SERVER_BASE}/templates" target/dist/public/app-server/templates
-  cp "${APP_SERVER_BASE}/package.json" target/dist/public/app-server/package.json
-  cd target/dist/public/app-server
-  npm install --production --loglevel=verbose
+  cp -R "${app_server_base_dir}/assets/" ./target/dist/public/app-server/assets/
+  cp -R "${app_server_base_dir}/bin/" ./target/dist/public/app-server/bin/
+  cp -R "${app_server_base_dir}/src/" ./target/dist/public/app-server/src/
+  cp -R "${app_server_base_dir}/templates/" ./target/dist/public/app-server/templates/
+  cp "${app_server_base_dir}/package.json" ./target/dist/public/app-server/package.json
+  cd ./target/dist/public/app-server
+  npm install --production --no-bin-links --loglevel=verbose
   cd -
 }
 
@@ -41,45 +40,45 @@ function bundle() {
   NODE_ENV=production ./node_modules/.bin/build-tools bundle
 }
 
-function installRuntimeDependencies() {
+function install_runtime_dependencies() {
   echo "Installing runtime dependencies"
   STRIPPED_PACKAGE="$(cat package.json | jq 'del(.devDependencies)' | jq '. | delpaths([paths|select(.[1]|tostring|test("@franklin.*"))])')"
-  echo "${STRIPPED_PACKAGE}" > target/dist/public/package.json
-  cd target/dist/public
-  npm install --production --loglevel=verbose
+  echo "${STRIPPED_PACKAGE}" > ./target/dist/public/package.json
+  cd ./target/dist/public
+  npm install --production --no-bin-links --loglevel=verbose
   cd -
 }
 
-function copyAssets() {
+function copy_assets() {
   echo "Copying assets"
-  if [[ -d assets/ ]]; then
-    cp -R assets/ target/dist/public/assets/
+  if [[ -d ./assets/ ]]; then
+    cp -R --no-target-directory ./assets/ ./target/dist/public/assets
   fi
-  if [[ -d mockData/ ]]; then
-    cp -R mockData/ target/dist/public/mockData/
+  if [[ -d ./mockData/ ]]; then
+    cp -R ./mockData/ ./target/dist/public/mockData
   fi
 }
 
-function createArchive() {
-  if [[ -z "${PACKAGE_NAME}" ]]; then
-    PACKAGE_NAME=$(cat target/dist/public/package.json | jq -r .name | sed -E 's/@([a-z]+)\///g')
+function create_archive() {
+  if [[ -z "${package_name}" ]]; then
+    package_name=$(cat ./target/dist/public/package.json | jq -r .name | sed -E 's/@([a-z]+)\///g')
   fi
   if [[ -n "${PROJECT_VERSION}" ]]; then
-    PACKAGE_NAME="${PACKAGE_NAME}-${PROJECT_VERSION}"
-    echo ${PROJECT_VERSION} > "target/dist/public/version.txt"
+    package_name="${package_name}-${PROJECT_VERSION}"
+    echo ${PROJECT_VERSION} > ./target/dist/public/version.txt
   fi
-  ARCHIVE_NAME="target/dist/${PACKAGE_NAME}.tar.gz"
-  echo "Creating archive ${ARCHIVE_NAME}"
-  tar -czvf "${ARCHIVE_NAME}" -C target/dist/public .
-  echo "Created archive ${ARCHIVE_NAME}"
+  archive_name="./target/dist/${package_name}.tar.gz"
+  echo "Creating archive ${archive_name}"
+  tar -czvf "${archive_name}" -C ./target/dist/public .
+  echo "Created archive ${archive_name}"
 }
 
 echo "Packaging $(cat package.json | jq -r .name)"
 
 verify
 clean
-installAppServer
+install_app_server
 bundle
-installRuntimeDependencies
-copyAssets
-createArchive
+install_runtime_dependencies
+copy_assets
+create_archive
