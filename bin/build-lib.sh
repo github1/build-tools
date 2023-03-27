e69a25dcb01a65b88bff874ff047e4d91c7680a8
github1 <john.collier4@gmail.com>
2023-03-27 01:53:21 +0000

    build script updates

--- bin/build-lib.sh
+++ bin/build-lib.sh
@@ -5,33 +5,74 @@ readlink_f() {
 }
 
 SCRIPTPATH=$(readlink_f ${0} | xargs dirname)
-DIST=./dist
 
-rm -rf "${DIST}"
+function log() {
+  >&2 echo "$PROJECT_NAME - $@"
+}
 
-if [[ -f "tsconfig.json" ]]; then
-    TSC=$(dirname $(dirname $(node -p "require.resolve('typescript')")))/bin/tsc
-    "${TSC}" --pretty --project tsconfig.json --outDir "${DIST}"
-    "${TSC}" --pretty --project tsconfig.json --module "commonjs" --outDir "${DIST}/es5"
-else
-    IGNORE="src/**/*.test.js"
-    cp "${SCRIPTPATH}/../config/babelrc.json" .babelrc
-    ${SCRIPTPATH}/../node_modules/.bin/babel src \
-        --out-dir "${DIST}" \
-        --ignore "${IGNORE}"
-    sed -i '' 's|false|"commonjs"|g' .babelrc
-    ${SCRIPTPATH}/../node_modules/.bin/babel src \
-        --out-dir "${DIST}/es5" \
-        --ignore "${IGNORE}"
-    rm .babelrc
-fi
+function clean() {
+  log "clean"
+  rm -rf "${DIST}"
+}
 
-MAIN=$(cat package.json | jq -r '.main' | sed 's|dist/||g' | sed 's|src/||g')
-cat package.json | jq '.main = "es5/'"${MAIN}"'" | .module = "'"${MAIN}"'"' > "${DIST}/package.json"
-if [[ -d "./bin" ]]; then
-  cp -R ./bin "${DIST}/bin"
-fi
+function transpile() {
+  log "transpile"
+  if [[ "${PROJECT_TYPE}" == 'ts' ]]; then
+      TSDIR=$(dirname $(dirname $(node -p "require.resolve('typescript')")))
+      log "using typescript version $(cat "${TSDIR}/package.json" | jq -r .version) from ${TSDIR}"
+      TSC="${TSDIR}/bin/tsc"
+      "${TSC}" --pretty --project tsconfig.json --outDir "${DIST}"
+      "${TSC}" --pretty --project tsconfig.json --module "commonjs" --outDir "${DIST}/es5"
+  else
+      IGNORE="src/**/*.test.js"
+      cp "${SCRIPTPATH}/../config/babelrc.json" .babelrc
+      ${SCRIPTPATH}/../node_modules/.bin/babel src \
+          --out-dir "${DIST}" \
+          --ignore "${IGNORE}"
+      sed -i '' 's|false|"commonjs"|g' .babelrc
+      ${SCRIPTPATH}/../node_modules/.bin/babel src \
+          --out-dir "${DIST}/es5" \
+          --ignore "${IGNORE}"
+      rm .babelrc
+  fi
+}
 
-if [[ -f "${AUX_PREPARE_SCRIPT}" ]]; then
-  "${AUX_PREPARE_SCRIPT}" "${DIST}/"
-fi
+function prepareDist() {
+  log "prepare dist"
+
+  local l_MAIN=$(cat package.json | jq -r ".main" | sed 's|dist/||g' | sed 's|src/||g' | sed 's/\.ts$/.js/g')
+  if [[ -z "${VAL}" ]] || [[ "${VAL}" == 'null' ]]; then
+    l_MAIN="index.js"
+  fi
+  PACKAGE_JSON=$(echo "${PACKAGE_JSON}" | jq '.main = "es5/'"${l_MAIN}"'" | .module = "'"${l_MAIN}"'"')
+  if [[ "${PROJECT_TYPE}" == 'ts' ]]; then
+    local l_TYPES=$(echo "${l_MAIN}" | sed 's/\.js$/.d.ts/g')
+    PACKAGE_JSON=$(echo "${PACKAGE_JSON}" | jq '.types = "'"${l_TYPES}"'"')
+  fi
+
+  log "writing package.json"
+  echo "${PACKAGE_JSON}" > "${DIST}/package.json"
+
+  if [[ -d "./bin" ]]; then
+    cp -R ./bin "${DIST}/bin"
+  fi
+}
+
+function main() {
+  DIST=./dist
+  PACKAGE_JSON=$(cat package.json)
+  PROJECT_NAME=$(echo "${PACKAGE_JSON}" | jq -r .name)
+  PROJECT_TYPE=js
+  if [[ -f "tsconfig.json" ]]; then
+    PROJECT_TYPE=ts
+  fi
+  clean
+  transpile
+  prepareDist
+  if [[ -f "${AUX_PREPARE_SCRIPT}" ]]; then
+    log "running AUX_PREPARE_SCRIPT ${AUX_PREPARE_SCRIPT}"
+    "${AUX_PREPARE_SCRIPT}" "${DIST}/"
+  fi
+}
+
+main
